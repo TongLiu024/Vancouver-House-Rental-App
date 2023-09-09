5aada7f1bd3539a9f26098c1058610f762661dc5
tliu024 <72672078+Huanying1996@users.noreply.github.com>
2023-09-08 23:37:54 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,jsx,tsx,ts}',
+    './pages/**/*.{js,jsx,tsx,ts}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
